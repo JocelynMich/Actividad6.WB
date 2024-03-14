7a5c03691419f5b7860b106830ef0cfb3d68511c
JocelynMich <98039944+JocelynMich@users.noreply.github.com>
2024-03-14 17:32:37 -0600

    Cambio a index

--- index.js
+++ index.js
@@ -2,21 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host:"localhost",
-  user:'root',
-  password:'K1m_D0kja20KAJ2M',
-  database: 'DiseñoAplicacionesWeb'
+index.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-connection.connect((error) => {
-  if (error) {
-    console.error('Error al conectar a la base de datos:', error);
-    return;
-  }
-  console.log('Conexión a la base de datos MySQL establecida correctamente.');
+index.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
-
-module.exports = connection;
